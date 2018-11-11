87579ff8ca5bd8bcba97034b89a7923a33c528ac
unknown <jodimcarlson4@gmail.com>
2018-11-11 12:47:09 -0600

    added java

---
+++ javascript.js
@@ -0,0 +1,15 @@
+$(document).ready (function(){
+$("#hangmanButton").on('click', function(){
+    window.location = "https://jmcarlson4.github.io/word-guess-game/";
+});
+
+$("#triviaButton").on('click', function(){
+    window.location = "https://jmcarlson4.github.io/TriviaGame/";
+});
+$("#gameButton").on('click', function(){
+    window.location = "https://jmcarlson4.github.io/Unit-4Game/";
+});
+$("#giphyButton").on('click', function(){
+    window.location = "https://jmcarlson4.github.io/GIPHY-API/";
+});
+});
